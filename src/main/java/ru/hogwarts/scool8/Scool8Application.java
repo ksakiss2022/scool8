4a0b4404357eb61a54ac99591827b448ed861bd0
Swetlana <sweta_zaika@mail.ru>
2023-01-29 19:59:13 +0300

    scool6

---
+++ src/main/java/ru/hogwarts/scool8/Scool8Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.scool8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scool8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scool8Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/scool8/Scool8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.scool8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scool8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
