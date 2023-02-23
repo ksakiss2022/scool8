352410d90d618b70944bf0a5be5bcd7f2f074ade
Swetlana <sweta_zaika@mail.ru>
2023-02-23 23:43:52 +0300

    scool8-Логирование и конфигурирование в приложении 23.02.2023-2

--- src/main/java/ru/hogwarts/scool8/service/StudentService.java
+++ src/main/java/ru/hogwarts/scool8/service/StudentService.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Service
-public class StudentService implements StudentServiceInterface {
+public class StudentService {
     private final Logger logger= LoggerFactory.getLogger(StudentService.class);
     private final StudentRepository studentRepository;
 
--- src/main/java/ru/hogwarts/scool8/service/StudentServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package ru.hogwarts.scool8.service;
-
-import ru.hogwarts.scool8.model.Student;
-
-public interface StudentServiceInterface {
-    Student createStudent(Student student);
-}
