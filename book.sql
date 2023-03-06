bbed19f8fc533853b497f5b84955bff714b2f9bc
Swetlana <sweta_zaika@mail.ru>
2023-03-06 23:40:15 +0300

    scool8-4.6  решение задачи к билету 7, выводим книги по ай ди автора+sql

---
+++ book.sql
@@ -0,0 +1,16 @@
+select*
+from book
+order by  author_id;
+
+select *
+from book
+where book.author_id=1;
+
+select *
+from book
+where book.author_id=2;
+
+select *
+from book
+where book.author_id=3;
+
